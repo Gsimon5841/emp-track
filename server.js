1b7f965359104af93786860e3530976c015c293b
Remnant <georgesimon4801@gmail.com>
2022-03-28 10:14:28 -0400

    mysqlconnect

---
+++ server.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Simo4801Emmanuel!',
+      database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+  );
+  
